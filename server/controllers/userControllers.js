7f6284893827fd5d0c141f55f8445f16a63ff5a6
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-04-06 15:47:29 +0200

    Added Schema from mongoose

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,42 +1,42 @@
 // REGISTER NEW USER
 // POST: api/users/register
 // UNPROTECTED
-const registerUser = (req, res, next) => {
+const registerUser = async (req, res, next) => {
 	res.json("Register User");
 };
 
 // LOGIN A REGISTERED USER
 // POST: api/users/login
 // UNPROTECTED
-const loginUser = (req, res, next) => {
+const loginUser = async (req, res, next) => {
 	res.json("Login User");
 };
 
 // USER PROFILE
 // POST: api/users/id
 // PROTECTED
-const getUser = (req, res, next) => {
+const getUser = async (req, res, next) => {
 	res.json("User Profile");
 };
 
 // CHANGE USER AVATAR (profile pic)
 // POST: api/users/change-avatar
 // PROTECTED
-const changeAvatar = (req, res, next) => {
+const changeAvatar = async (req, res, next) => {
 	res.json("Change User Avatar");
 };
 
 // ADIT USER DETAILS (from profile)
 // POST: api/users/edit-user
 // PROTECTED
-const editUser = (req, res, next) => {
+const editUser = async (req, res, next) => {
 	res.json("Edit user details");
 };
 
 // GET AUTHORS
 // POST: api/users/authors
 // UNPROTECTED
-const getAuthors = (req, res, next) => {
+const getAuthors = async (req, res, next) => {
 	res.json("Get all users/authors");
 };
 
