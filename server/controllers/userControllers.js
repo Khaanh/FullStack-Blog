fd83dbd0d9e3b5ca1398f03d40065aa5a008c60e
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-04-08 15:58:55 +0200

    Register User

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,8 +1,45 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const HttpError = require("../models/errorModel");
+
 // REGISTER NEW USER
 // POST: api/users/register
 // UNPROTECTED
 const registerUser = async (req, res, next) => {
-	res.json("Register User");
+	try {
+		const { name, email, password, password2 } = req.body;
+		if (!name || !email || !password) {
+			return next(new HttpError("Fill in all fields.", 422));
+		}
+
+		const newEmail = email.toLowerCase();
+		const emailExists = await User.findOne({ email: newEmail });
+
+		if (emailExists) {
+			return next(new HttpError("Email already exists", 422));
+		}
+
+		if (password.trim().length < 6) {
+			return next(
+				new HttpError("Password should be at least 6 characters.", 422)
+			);
+		}
+
+		if (password != password2) {
+			return next(new HttpError("Password do not match.", 422));
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPass = await bcrypt.hash(password, salt);
+		const newUser = await User.create({
+			name,
+			email: newEmail,
+			password: hashedPass,
+		});
+		res.status(201).json(`New user ${newUser.email} registered.`);
+	} catch (error) {
+		return next(new HttpError("User registration failed.", 422));
+	}
 };
 
 // LOGIN A REGISTERED USER
